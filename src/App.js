f8b5069053ab895974c0be79a3b936bb2137118b
Danqing Yang <lenayoungydq@gmail.com>
2022-07-29 17:18:35 +0200

    CRA

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const cities = [
+    {id:1, name: "Chicago"},
+    {id:1, name: "New York City"},
+    {id:1, name: "Miami"},
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>React 18 no Suspense</h1>  
     </div>
   );
 }
